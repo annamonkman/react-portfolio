b7f4542371b7cc7f8f4d5b195e21cfef00329bb9
Anna Monkman <annamonkman@me.com>
2021-08-24 16:38:00 +0100

    added content and css

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -8,25 +8,42 @@ const About = () => {
       <div className="dot-about">
         <div className="content-about">
 
-          <Fade top>
-            <div className="bio-wrapper">
-              <p>I am a creative and perceptive junior web developer with a background in the Visual Arts who has recently completed a Software Engineering Bootcamp at General Assembly.</p>
-              <p>I graduated from a BA Fine Art Degree with First Class Honours in 2019. During my studies I specialised in using digital filmmaking and animation software. I enjoy the process of developing a visual and conceptual outcome using aesthetic decision-making, technical skills and problem solving.  Learning to code was top of my list of skills to develop after graduating, as it is a skill that enables many avenues for creation and innovation.</p>
-            </div>
-          </Fade>
+          
+          <div className="bio-wrapper">
+
+            <Fade top>
+              <div className="bio-p-1">
+                <p>I am a creative and perceptive junior web developer with a background in the Visual Arts who has recently completed a Software Engineering Bootcamp at General Assembly.</p>
+              </div>
+            </Fade>
+            <Fade top>
+              <div className="bio-p-2">
+                <p>I graduated from a BA Fine Art Degree with First Class Honours in 2019. During my studies I specialised in using digital filmmaking and animation software. I enjoy the process of developing a visual and conceptual outcome using aesthetic decision-making, technical skills and problem solving.  Learning to code was top of my list of skills to develop after graduating, as it is a skill that enables many avenues for creation and innovation.</p>
+              </div>
+            </Fade>
+            <Fade top>
+              <div className="bio-p-3">
+                <p>I completed an Introduction to Web Development course at Code First Girls in 2020 which solidified my desire to become a Software Engineer. Wanting to enhance my skills to a higher level, I enrolled into the Software Engineering Bootcamp at General Assembly. I hope to work in an environment in which I can continually learn and be challenged, whether that&apos;s in a team or individually, as well as apply my skills, in particular my eye for detail and creative thinking.</p>
+              </div>
+            </Fade>
+            
+          </div>
           
           
           <Fade top>
             <div className="skills-wrapper">
+              <div className="skills-title"><h3>Skills</h3></div>
+              <div className="frontend-title">
+                <h4>Front end:</h4>
+              </div>
               <div className="frontend-wrapper">
-
                 <div className="skill-indv-wrapper">
                   <div className="skill-indv-img"></div>
                   <div className="skill-indv-title">HTML5</div>
                 </div>
                 <div className="skill-indv-wrapper">
                   <div className="skill-indv-img"></div>
-                  <div className="skill-indv-title">CSS3</div>
+                  <div className="skill-indv-title">CSS</div>
                 </div>
                 <div className="skill-indv-wrapper">
                   <div className="skill-indv-img"></div>
@@ -50,7 +67,9 @@ const About = () => {
                 </div>
             
               </div>
-
+              <div className="frontend-title">
+                <h4>Backend:</h4>
+              </div>
               <div className="frontend-wrapper">
                 
                 <div className="skill-indv-wrapper">
@@ -79,7 +98,7 @@ const About = () => {
                 </div>
                 <div className="skill-indv-wrapper">
                   <div className="skill-indv-img"></div>
-                  <div className="skill-indv-title">PostgresSQL</div>
+                  <div className="skill-indv-title">PostgreSQL</div>
                 </div>
                 <div className="skill-indv-wrapper">
                   <div className="skill-indv-img"></div>
@@ -96,6 +115,10 @@ const About = () => {
             
               </div>
 
+              <div className="frontend-title">
+                <h4>Packages:</h4>
+              </div>
+
               <div className="frontend-wrapper">
                 
                 <div className="skill-indv-wrapper">
@@ -104,7 +127,7 @@ const About = () => {
                 </div>
                 <div className="skill-indv-wrapper">
                   <div className="skill-indv-img"></div>
-                  <div className="skill-indv-title">NPM</div>
+                  <div className="skill-indv-title">Npm</div>
                 </div>
                 <div className="skill-indv-wrapper">
                   <div className="skill-indv-img"></div>
@@ -116,9 +139,81 @@ const About = () => {
                 </div>
                 
               </div>
-              
-              <p>Git, GitHub, Insomnia, VS Code, TablePlus, Cloudinary, Mapbox, Netlify, Heroku</p>
-              <p>Adobe Photoshop, Adobe Premiere, Blender, Unreal Engine</p>
+
+              <div className="frontend-title">
+                <h4>Tools:</h4>
+              </div>
+
+              <div className="frontend-wrapper">
+                
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Git</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">GitHub</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Insomnia</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">VS Code</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">TablePlus</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Cloudinary</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Mapbox</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Netlify</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Heroku</div>
+                </div>
+                
+              </div>
+
+              <div className="frontend-title">
+                <h4>Art & Design:</h4>
+              </div>
+
+              <div className="frontend-wrapper">
+                
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Adobe Photoshop</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Adobe Premiere</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Blender</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Unreal Engine</div>
+                </div>
+                <div className="skill-indv-wrapper">
+                  <div className="skill-indv-img"></div>
+                  <div className="skill-indv-title">Figma</div>
+                </div>
+                
+              </div>
+
             </div>
           </Fade>
 
@@ -128,24 +223,29 @@ const About = () => {
                 <h2>Experience & Education</h2>
               </div>
               <div className="experience-list">
+
                 <div className="experience-indv">
-                  <p>Waitrose, Supermarket assistant</p>
+                  <p><span className="ex-position">Supermarket Assistant</span><span className="ex-company">Waitrose, Godalming</span><span className="ex-dates">PT Nov 2020 - Current</span></p>
                 </div>
                 <div className="experience-indv">
-                  <p>General Assembly Software Engineering Immersive</p>
+                  <p><span className="ex-position">Software Engineering Immersive</span><span className="ex-company">General Assembly, London/Remote</span><span className="ex-dates">Feb 2021 - Apr 2021</span></p>
                 </div>
                 <div className="experience-indv">
-                  <p>Runner, One of Us</p>
+                  <p><span className="ex-position">Runner</span><span className="ex-company">One of Us, London</span><span className="ex-dates">Feb 2020 - Aug 2020</span></p>
                 </div>
                 <div className="experience-indv">
-                  <p>Code First Girls</p>
+                  <p><span className="ex-position">Introduction to Web Development</span><span className="ex-company">Code First Girls, London</span><span className="ex-dates">Jan 2020 - March 2020 PT</span></p>
                 </div>
                 <div className="experience-indv">
-                  <p>Goldsmiths University of London</p>
+                  <p><span className="ex-position">Work Placement</span><span className="ex-company">Furtherfield Gallery, London</span><span className="ex-dates">May 2019 - Oct 2019</span></p>
                 </div>
                 <div className="experience-indv">
-                  <p>Kingston School of Art</p>
+                  <p><span className="ex-position">BA Fine Art</span><span className="ex-company">Goldsmiths, University of London</span><span className="ex-dates">Sept 2016 - June 2019 (First Class Honors)</span></p>
                 </div>
+                <div className="experience-indv">
+                  <p><span className="ex-position">Foundation Diploma in Art & Design</span><span className="ex-company">Kingston, School of Art</span><span className="ex-dates">Sept 2015 - June 2016</span></p>
+                </div>
+
               </div>
             </div>
           </Fade>
