8b82f7fc40118c3e1f8cfdb36e3a7414ed48db5d
Anna Monkman <annamonkman@me.com>
2023-11-01 15:24:10 +0000

    add text to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <div className="home-container">
       <img className="home-image" src="https://res.cloudinary.com/project-3/image/upload/v1698769873/portfolio23/foliohome02_ggv4hy.png" alt="home image"></img>
-      {/* <div className="home-info">
-        <p>Anna Monkman</p>
+      <div className="home-info">
+        <p>Anna Monkman.</p>
         <p>London based artist</p>
-      </div> */}
+      </div>
     </div>
   )
 }
